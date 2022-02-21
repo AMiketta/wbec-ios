db61729b3b07b600a860c269c4a69a43929ecb6e
Andreas Miketta <andreas.miketta@hmmh.de>
2022-02-21 07:22:14 +0100

    Initial Commit

---
+++ wbec-ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wbec-ios-app
+//
+//  Created by Andreas Miketta on 21.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wbec-ios-app/wbec_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wbec_ios_appApp.swift
+//  wbec-ios-app
+//
+//  Created by Andreas Miketta on 21.02.22.
+//
+
+import SwiftUI
+
+@main
+struct wbec_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
